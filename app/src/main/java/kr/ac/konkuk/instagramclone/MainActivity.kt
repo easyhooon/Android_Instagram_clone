063fcbd92a9097b250605b873fd31ecb9c198b41
easyhooon <51016231+easyhooon@users.noreply.github.com>
2021-01-20 04:59:37 +0900

    commit until 13 Chapter

--- app/src/main/java/kr/ac/konkuk/instagramclone/MainActivity.kt
+++ app/src/main/java/kr/ac/konkuk/instagramclone/MainActivity.kt
@@ -1,47 +1,36 @@
 package kr.ac.konkuk.instagramclone
 
-import Fragments.NotificationsFragment
-import Fragments.ProfileFragment
-import Fragments.SearchFragment
-import android.app.Notification
-import android.content.Intent
+import kr.ac.konkuk.instagramclone.Fragments.*
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import Fragments.homeFragment
 
 class MainActivity : AppCompatActivity() {
-
-    internal var selectedFragment: Fragment ? = null
 
 
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.nav_home -> {
-                selectedFragment = homeFragment()
+                moveToFragment(HomeFragment())
+                return@OnNavigationItemSelectedListener true //화면 전환 (하이라이트?)
             }
             R.id.nav_search -> {
-                selectedFragment = SearchFragment()
+                moveToFragment(SearchFragment())
+                return@OnNavigationItemSelectedListener true
             }
             R.id.nav_add_post -> {
                 return@OnNavigationItemSelectedListener true
             }
             R.id.nav_notifications -> {
-                selectedFragment = NotificationsFragment()
+                moveToFragment(NotificationsFragment())
+                return@OnNavigationItemSelectedListener true
+
             }
             R.id.nav_profile -> {
-                selectedFragment = ProfileFragment()
+                moveToFragment(ProfileFragment())
+                return@OnNavigationItemSelectedListener true
             }
-        }
-
-        if(selectedFragment != null)
-        {
-            supportFragmentManager.beginTransaction().replace(
-                R.id.fragment_container,
-                selectedFragment!!
-            ).commit()
         }
 
         false
@@ -56,22 +45,14 @@ class MainActivity : AppCompatActivity() {
 
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
 
+        moveToFragment(HomeFragment()) //앱을 시작할때 디폴트로 홈 프래그먼트가 켜지도록
 
-        //왜 있는건지 알기
-        supportFragmentManager.beginTransaction().replace(
-            R.id.fragment_container,
-            homeFragment()
-        ).commit()
-
-
-//        moveToFragment(HomeFragment())
     }
 
-
-//    private fun moveToFragment(fragment: Fragment)
-//    {
-//        val fragmentTrans = supportFragmentManager.beginTransaction()
-//        fragmentTrans.replace(R.id.fragment_container, fragment)
-//        fragmentTrans.commit()
-//    }
+    private fun moveToFragment(fragment: Fragment)
+    {
+        val fragmentTrans = supportFragmentManager.beginTransaction() //fragement Transaction
+        fragmentTrans.replace(R.id.fragment_container, fragment)
+        fragmentTrans.commit()
+    }
 }
